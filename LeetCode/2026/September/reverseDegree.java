bed953af8d7567edc426d90f411c375d6fc94f5e
Vaishnavi Moturi <moturi.vaishnavi2006@gmail.com>
2026-09-20 21:22:54 +0530

    Add reverseDegree method to calculate reverse degree

---
+++ LeetCode/2026/September/reverseDegree.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            ans+=(i+1)*(26-(s.charAt(i)-'a'));
+        }
+        return ans;
+    }
+}
